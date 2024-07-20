3bfc031c814668edfaac32dee30a71c8b818e78b
Atharva Naik <atharvan.coder@gmail.com>
2024-07-20 16:08:46 +0530

    01js

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let num = 0 ;
+    numbers.forEach(element => {
+        if(num < element)
+            num = element;
+    });
+    return num;
 }
 
+console.log(findLargestElement([30, 7, 21, 9, 1111]));
 module.exports = findLargestElement;
